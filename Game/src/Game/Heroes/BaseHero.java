77354672ae093dad0304fd69085b7d36a7be9949
AKChizhov <ChizhovAK2007@yandex.ru>
2023-02-12 20:32:08 +0300

    +2+Добавлены игроки в  шаблон ДЗ к семинару №1

--- Game/src/Game/Heroes/BaseHero.java
+++ Game/src/Game/Heroes/BaseHero.java
@@ -3,10 +3,10 @@ package Game.Heroes;
 public abstract class BaseHero {
     
     protected String nameCharacter;
-    int damage;
-    int healthPoints;
-    int defence;
-    int evolution;
+    protected int damage;
+    protected int healthPoints;
+    protected int defence;
+    protected int evolution;
 
     public BaseHero(String nameCharacter,int damage,int healthPoints,int defence,int evolution) {
         this.nameCharacter = nameCharacter;
--- Game/src/Game/Heroes/Peasant.java
+++ Game/src/Game/Heroes/Peasant.java
@@ -3,7 +3,7 @@ package Game.Heroes;
 public class Peasant extends BaseHero{
 
     public Peasant(String nameCharacter, int damage, int healthPoints, int defence, int evolution) {
-        super(nameCharacter, damage, 100, 100, evolution);
+        super(nameCharacter, damage, 100, 100, 0);
         
     }
 
